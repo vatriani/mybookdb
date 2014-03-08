6ad344e38447db7d93d542072d93bd84f0d37533
Niels Neumann <vatriani.nn@googlemail.com>
2014-03-08 14:47:54 +0100

    adding language translations

---
+++ translation/language_de.ts
@@ -0,0 +1,280 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="15"/>
+        <source>MyBookDB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="73"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="79"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="90"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="95"/>
+        <source>&amp;add Book</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="100"/>
+        <source>&amp;edit Book</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="105"/>
+        <source>remove Book</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="110"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="115"/>
+        <location filename="../src/ui/mainwindow.ui" line="130"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="120"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="125"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="71"/>
+        <location filename="../src/ui/mainwindow.cpp" line="72"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="73"/>
+        <source>Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="74"/>
+        <location filename="../src/ui/mainwindow.cpp" line="75"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="76"/>
+        <source>Scan from Barcode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="87"/>
+        <source>Main ToolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>bookdescription</name>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="46"/>
+        <source>ISBN:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="78"/>
+        <source>Title:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="88"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="101"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="139"/>
+        <source>Publishdate:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="149"/>
+        <source>Publisher:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="159"/>
+        <source>Collection:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="172"/>
+        <source>Part:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="179"/>
+        <source>Language:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="189"/>
+        <source>Pages:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="230"/>
+        <source>Authors:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="237"/>
+        <source>Genre:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="244"/>
+        <source>Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="257"/>
+        <source>Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="265"/>
+        <source>Readed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="289"/>
+        <source>Added date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="296"/>
+        <source>Translated:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="306"/>
+        <source>Translator:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="316"/>
+        <source>Rating:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="323"/>
+        <source>Print version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="333"/>
+        <source>Format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="343"/>
+        <source>Buying date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="350"/>
+        <source>Comments:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="369"/>
+        <source>Rent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="375"/>
+        <source>Customer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="385"/>
+        <source>Rent date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="395"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="406"/>
+        <source>Marks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="412"/>
+        <source>My Favorit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="427"/>
+        <location filename="../src/ui/bookdescription.ui" line="434"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fieldManager</name>
+    <message>
+        <location filename="../src/ui/fieldmanager.cpp" line="42"/>
+        <source>add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/fieldmanager.cpp" line="44"/>
+        <source>remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>overview</name>
+    <message>
+        <location filename="../src/ui/overview.ui" line="14"/>
+        <source>Collection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/overview.ui" line="40"/>
+        <source>Groups</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/overview.ui" line="84"/>
+        <source>Filters</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translation/language_en.ts
@@ -0,0 +1,327 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="15"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="117"/>
+        <source>MyBookDB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="73"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="127"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="79"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="128"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="90"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="118"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="95"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="119"/>
+        <source>&amp;add Book</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="100"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="120"/>
+        <source>&amp;edit Book</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="105"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="121"/>
+        <source>remove Book</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="110"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="122"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="115"/>
+        <location filename="../src/ui/mainwindow.ui" line="130"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="123"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="126"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="120"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="124"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.ui" line="125"/>
+        <location filename="../bin/src/ui_mainwindow.h" line="125"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="71"/>
+        <location filename="../src/ui/mainwindow.cpp" line="72"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="73"/>
+        <source>Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="74"/>
+        <location filename="../src/ui/mainwindow.cpp" line="75"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="76"/>
+        <source>Scan from Barcode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainwindow.cpp" line="87"/>
+        <source>Main ToolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>bookdescription</name>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="14"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="469"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="46"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="470"/>
+        <source>ISBN:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="78"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="471"/>
+        <source>Title:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="88"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="472"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="101"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="483"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="139"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="474"/>
+        <source>Publishdate:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="149"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="475"/>
+        <source>Publisher:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="159"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="476"/>
+        <source>Collection:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="172"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="477"/>
+        <source>Part:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="179"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="478"/>
+        <source>Language:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="189"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="479"/>
+        <source>Pages:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="230"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="480"/>
+        <source>Authors:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="237"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="481"/>
+        <source>Genre:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="244"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="482"/>
+        <source>Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="257"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="493"/>
+        <source>Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="265"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="484"/>
+        <source>Readed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="289"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="485"/>
+        <source>Added date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="296"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="486"/>
+        <source>Translated:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="306"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="487"/>
+        <source>Translator:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="316"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="488"/>
+        <source>Rating:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="323"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="489"/>
+        <source>Print version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="333"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="490"/>
+        <source>Format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="343"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="491"/>
+        <source>Buying date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="350"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="492"/>
+        <source>Comments:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="369"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="497"/>
+        <source>Rent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="375"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="494"/>
+        <source>Customer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="385"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="495"/>
+        <source>Rent date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="395"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="496"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="406"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="501"/>
+        <source>Marks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="412"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="498"/>
+        <source>My Favorit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/bookdescription.ui" line="427"/>
+        <location filename="../src/ui/bookdescription.ui" line="434"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="499"/>
+        <location filename="../bin/src/ui_bookdescription.h" line="500"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fieldManager</name>
+    <message>
+        <location filename="../src/ui/fieldmanager.cpp" line="42"/>
+        <source>add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/fieldmanager.cpp" line="44"/>
+        <source>remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>overview</name>
+    <message>
+        <location filename="../src/ui/overview.ui" line="14"/>
+        <location filename="../bin/src/ui_overview.h" line="92"/>
+        <source>Collection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/overview.ui" line="40"/>
+        <location filename="../bin/src/ui_overview.h" line="93"/>
+        <source>Groups</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/overview.ui" line="84"/>
+        <location filename="../bin/src/ui_overview.h" line="94"/>
+        <source>Filters</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
